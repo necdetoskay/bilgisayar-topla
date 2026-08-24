51bca2ac329bfe053ed9bae345fb1fb746631ad6
necdetoskay <necdetoskay@hotmail.com>
2026-08-24 22:09:28 +0300

    feat(requirements): export resolution pipeline

--- packages/requirements/src/public.ts
+++ packages/requirements/src/public.ts
@@ -2,3 +2,4 @@ export * from "./index.js";
 export * from "./official-source-registry.js";
 export * from "./official-source-resolver.js";
 export * from "./requirement-extraction.js";
+export * from "./requirement-resolution-pipeline.js";
